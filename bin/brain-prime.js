7219f91830972c342cf7dbcf63a144b55f9c95d0
Artem Donchenko <anderleht00712@gmail.com>
2022-09-08 16:38:40 +0300

    some minor fixes on the project

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import brainprime from '../src/games/brain-prime.js';
+import getQuestion from '../src/games/brain-prime.js';
 import GetanAnswer from '../src/index.js';
 
 const gamerule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-GetanAnswer(gamerule, brainprime());
+GetanAnswer(gamerule, getQuestion());
